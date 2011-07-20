22fa6b461bd4a6c218eff0dde1a90f03ba06f1f9
Vincent <vinzha@gmail.com>
2011-07-20 16:04:11 -0400

    up to pt4 in android development

--- src/com/vincent/demo/StatusActivity.java
+++ src/com/vincent/demo/StatusActivity.java
@@ -1,16 +1,16 @@
 package com.vincent.demo;
 
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusActivity extends Activity{
 	EditText editStatus;
-	Button buttonUpdate;
 	
     /** Called when the activity is first created. */
     @Override
@@ -18,13 +18,26 @@ public class StatusActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status);
         editStatus = (EditText)findViewById(R.id.edit_status);
-        buttonUpdate = (Button)findViewById(R.id.button_update);
         
-        buttonUpdate.setOnClickListener(this);
     }
 
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		Log.d("Yamba", "onClicked");
+		String status = editStatus.getText().toString();
+		String result;
+		try{
+			Twitter twitter = new Twitter("student", "password");
+			twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+			twitter.setStatus(status);
+			result = "Message posted successfully";
+		}catch (TwitterException e){
+			result = "Failed posting status";
+			Log.e("yamba", "Can't Connect to Server", e);
+			//e.printStackTrace();
+		}
+		
+		Toast.makeText(this, result, Toast.LENGTH_LONG).show(); // must have .show() or it will not compile
+		
+		Log.d("Yamba", "onClicked "+status);
 	}
 }
